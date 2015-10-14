36e241be4f215ddba0d7ad9d76f462e24cff5bc9
johndrkurtcom <john@drkurt.com>
2015-10-14 12:11:52 -0700

    removed auth file from cache

--- server/config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  
-  'googleAuth' : {
-    'clientID' : process.env.clientID || 'nothing',
-    'clientSecret' : process.env.clientSecret || 'nothing',
-    'callbackURL' : process.env.callbackURL || 'http://localhost:3000/auth/google/callback'
-  }
-
-};
